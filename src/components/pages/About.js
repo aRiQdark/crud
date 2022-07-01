c87c3160aee4a364495c53a0b88d70d2c6ccd2ae
aRiQdark <92494645+aRiQdark@users.noreply.github.com>
2022-07-01 15:48:31 +0700

    code

---
+++ src/components/pages/About.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div className="container">
+      <div className="py-4">
+        <h1>About Page</h1>
+        <p className="lead">
+         Josua olan
+      
+        </p>
+
+        <p className="lead">
+        M.rizhan raditya
+        </p>
+        <p className="lead">   Muhammad Ariq Pratama</p>
+      </div>
+    </div>
+  );
+};
+
+export default About;
